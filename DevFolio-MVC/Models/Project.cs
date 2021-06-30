2b4ef398dbff9555315ac0be07bf695f8877e23f
shayan <182shayan@gamail.com>
2021-06-30 19:21:42 +0330

    Dynamic projects with View Component

---
+++ DevFolio-MVC/Models/Project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFolio_MVC.Models
+{
+    public class Project
+    {
+        public Project(int id, string image, string title, string category, string client, string date, string url, string description)
+        {
+            Id = id;
+            Image = image;
+            Title = title;
+            Category = category;
+            Client = client;
+            Date = date;
+            Url = url;
+            Description = description;
+        }
+
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Client { get; set; }
+        public string Date { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
