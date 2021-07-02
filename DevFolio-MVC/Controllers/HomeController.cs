bc38376dafa1480239295ff1213c8525254ea090
shayan <182shayan@gamail.com>
2021-07-02 15:29:22 +0330

    required server validation

--- DevFolio-MVC/Controllers/HomeController.cs
+++ DevFolio-MVC/Controllers/HomeController.cs
@@ -18,10 +18,17 @@ namespace DevFolio_MVC.Controllers
             return View(model);
         }
         [HttpPost]
-           public IActionResult Index(Message form)
-        {
-            return Json(Ok());
-        }
+          public IActionResult Index(Message form)
+          {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Error = "The information entered is incorrect ! please try again.";
+                return View(form);
+            }
+            ModelState.Clear();
+            ViewBag.Success = "Your message was sent successfully!";
+            return View();
+          }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
